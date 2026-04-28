fa0b873328a6d98cc4cfd126eea4e45f92021e01
imamnghr <imamn208@gmail.com>
2026-04-28 22:27:22 +0700

    create features reward redeem

--- src/pages/claimReward.jsx
+++ src/pages/claimReward.jsx
@@ -10,7 +10,18 @@ export default function CheckGates() {
   const [loading, setLoading] = useState(false);
 
   const handleScan = async (detectedCodes) => {
-   console.log(detectedCodes)
+    console.log(detectedCodes);
+    const params = {
+      token: detectedCodes[0].rawValue,
+    };
+    try {
+      const res = await api.get("/rewards/redeem", { params });
+      const { data } = res.data;
+      setResults(data);
+      toast.success("Redeem success");
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -37,46 +48,28 @@ export default function CheckGates() {
           <div className="w-full max-w-sm space-y-2">
             {/* Nama */}
             <div className="rounded-xl bg-white p-3 shadow">
-              <p className="text-xs text-gray-500">Nama</p>
-              <p className="font-semibold">{results.name}</p>
+              <p className="text-xs text-gray-500">Customer Name</p>
+              <p className="font-semibold">{results.user.name}</p>
             </div>
 
             {/* Phone */}
             <div className="rounded-xl bg-white p-3 shadow">
-              <p className="text-xs text-gray-500">No. HP</p>
-              <p className="font-semibold">{results.phone || "-"}</p>
+              <p className="text-xs text-gray-500">Phone Number</p>
+              <p className="font-semibold">{results.user.phone || "-"}</p>
             </div>
 
             {/* Booking ID */}
             <div className="rounded-xl bg-white p-3 shadow">
-              <p className="text-xs text-gray-500">Booking ID</p>
-              <p className="font-semibold">{results.booking_id}</p>
+              <p className="text-xs text-gray-500">Reward Category</p>
+              <p className="font-semibold">{results.reward.category}</p>
             </div>
 
             {/* Booking Type */}
             <div className="rounded-xl bg-white p-3 shadow">
-              <p className="text-xs text-gray-500">Tipe Booking</p>
-              <p className="font-semibold capitalize">{results.booking_type}</p>
-            </div>
-
-            {/* Class Name */}
-            <div className="rounded-xl bg-white p-3 shadow">
-              <p className="text-xs text-gray-500">Class</p>
-              <p className="font-semibold">{results.class_name || "-"}</p>
-            </div>
-
-            {/* Schedule Date */}
-            <div className="rounded-xl bg-white p-3 shadow">
-              <p className="text-xs text-gray-500">Tanggal</p>
-              <p className="font-semibold">
-                {moment(results.schedule_date).format("YYYY MM DD HH:mm")}
+              <p className="text-xs text-gray-500">Reward Name</p>
+              <p className="font-semibold capitalize">
+                {results.reward.item_name}
               </p>
-            </div>
-
-            {/* Message */}
-            <div className="rounded-xl bg-green-50 border border-green-200 p-3">
-              <p className="text-xs text-green-700">Status</p>
-              <p className="font-semibold text-green-800">{results.message}</p>
             </div>
           </div>
         )}
